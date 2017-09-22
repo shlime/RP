84dd38713ad8801acbc4db2414df6116787c8828
shlime <slimee@gmail.hu>
2017-09-22 09:00:27 +0200

    akocsi

--- server/mods/deathmatch/resources/erpe/server.lua
+++ server/mods/deathmatch/resources/erpe/server.lua
@@ -1,4 +1,5 @@
 root = getRootElement()
+MAX_PLAYERS = 32
 
 
 addEventHandler("onPlayerJoin", root,
@@ -7,16 +8,36 @@ addEventHandler("onPlayerJoin", root,
 		setCameraTarget(source, source)
 		fadeCamera(source, true)
 		outputChatBox("Szia barát! Üdv Diószeg legjobb erpé szerverén..", source)
+		setPlayerNametagShowing ( source, false ) 
 	end
 )
 
 addCommandHandler("skin",
-	function ( player, command, target, skin )
+	function ( player, command, targetID, skin )
+
 		local skin = tonumber(skin)
-		local target = tonumber(target)
-		local targetName = getPlayerFromID(target)
-		setElementModel(targetName, skin)
-		outputChatBox("Megváltoztattad " .. getPlayerName(targetName) .. " skinjét ID" .. skin .. "-re.", player)
-		outputChatBox("Megváltoztatták a skined " .. skin .. "-re.", targetName)		
+		local targetID = tonumber(targetID)
+		
+		if(targetID == nil or skin == nil) then return outputChatBox("Használat: /skin [ID] [SkinID]", player) end
+		if(targetID < 0 or targetID > MAX_PLAYERS) then return outputChatBox("ID 0-32", player) end
+		if(skin < 0 or skin > 312) then return outputChatBox("skin: 0-312", player) end
+
+		local target = getPlayerFromID(targetID)
+		setElementModel(target, skin)
+		outputChatBox("Megváltoztattad " .. getPlayerName(target) .. " skinjét ID" .. skin .. "-re.", player)
+		outputChatBox("Megváltoztatták a skined " .. skin .. "-re.", target)		
 	end
+)
+
+addCommandHandler("akocsi",
+	function(player, command, vehicleID)
+
+		local vehicleID = tonumber(vehicleID)
+		local x,y,z = getElementPosition(player)
+
+		local vehicle = createVehicle(vehicleID, 0,0,0)
+		spawnVehicle(vehicle, x+5,y,z+1)
+		if(spawnVehicle) then outputChatBox("Adminkocsi lehívva: ID" .. vehicleID, player) end
+	end
+
 )
